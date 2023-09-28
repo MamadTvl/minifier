8c317e0f394b2ef5caeadba380176e660523acba
MamadTvl <mtavallai80@gmail.com>
2023-09-28 13:47:19 +0330

    add validationPipe and swagger

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as mongoose from 'mongoose';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    const config = new DocumentBuilder()
+        .setTitle('Parspack Test')
+        .setDescription('API description')
+        .setVersion('1.0')
+        .addBearerAuth(
+            {
+                type: 'apiKey',
+                name: 'authorization',
+                in: 'header',
+            },
+            'user-auth',
+        )
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('doc', app, document);
     await app.listen(3000);
+    mongoose.set('debug', true);
 }
 bootstrap();
