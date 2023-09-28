1138961ebb96ba4e6b8c03a0f057e8849eef4c6f
MamadTvl <mtavallai80@gmail.com>
2023-09-28 13:49:46 +0330

    import new modules to app module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -4,18 +4,34 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration, { Config } from './config/configuration';
 
 @Module({
     imports: [
-        MongooseModule.forRoot('', {
-            auth: {
-                username: '',
-                password: '',
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+            load: [configuration],
+        }),
+        MongooseModule.forRootAsync({
+            useFactory: async (configService: ConfigService<Config>) => {
+                const conf = configService.get<Config['mongo']>('mongo');
+                return {
+                    uri: `mongodb://${conf.host}:${conf.port}/${conf.database}`,
+                    auth: {
+                        username: conf.user,
+                        password: conf.password,
+                    },
+                    authSource: conf.authSource,
+                };
             },
-            authSource: '',
+            inject: [ConfigService],
         }),
         UserModule,
         TaskModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
